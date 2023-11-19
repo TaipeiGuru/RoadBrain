2da4daf9cd018bb1ae52f05bb8544a0309392307
Ananya Pattamatta <ananyapattamatta@gmail.com>
2023-11-18 23:48:22 -0500

    React front end for our app

---
+++ road_brain_react/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Background from "./components/Background/Background";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+
+const App = () => {
+  let heroData = [
+    {text1:"Dive into", text2:"what you love"},
+    {text1:"Indulge", text2:"your passions"},
+    {text1:"Give into", text2:"your desires"},
+  ]
+  const [heroCount,setHeroCount] = useState(0);
+  const [playStatus,setPlayStatus] = useState(false);
+
+  useEffect(()=> {
+    setInterval(() => {
+      setHeroCount((count)=>{return count==2?0:count+1})
+    }, 5000);
+
+  },[])
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero 
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ road_brain_react/src/components/Background/Background.jsx
@@ -0,0 +1,27 @@
+import './Background.css'
+import video1 from '../../Assets/video1.mp4'
+import img1 from '../../Assets/image1.png'
+import img2 from '../../Assets/image2.png'
+import img3 from '../../Assets/image3.png'
+
+const Background = ({playStatus, heroCount}) => {
+    if(playStatus) {
+        return (
+            <video className="background fade-in" autoPlay loop muted>
+                <source src={video1} type='video/mp4'/>
+            </video>
+        )
+    }
+    else if(heroCount==0){
+        return <img src={img1} className='background' alt="" />
+    }
+    else if(heroCount==1){
+        return <img src={img2} className='background' alt="" />
+    }
+    else if(heroCount==2){
+        return <img src={img3} className='background' alt="" />
+    }
+}
+  
+
+export default Background
